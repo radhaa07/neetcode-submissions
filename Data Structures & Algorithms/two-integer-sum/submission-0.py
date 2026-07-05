e0b4d12b02ad6d1e05163b685b0bc8aa243e4988
Radha Waghmare <radhawaghmare498@gmail.com>
2026-07-05 17:04:27 +0530

    Delete Data Structures & Algorithms/two-integer-sum directory

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hm={}
-        for i in range(len(nums)):
-            ans = target - nums[i]
-            if ans in hm:
-                return [hm.get(ans), i]
-            hm[nums[i]] = i
-        
